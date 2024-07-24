773410ab51d3d02a9efe250f2ba80a5d0642da81
Michael Zakhary <michael.zakhary33@gmail.com>
2024-07-24 14:04:18 -0400

    exercise 1.5

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course}/>
+      <Header course={course.name}/>
       <Content 
-      p1={parts[0].name} e1={parts[0].exercises} 
-      p2={parts[1].name} e2={parts[1].exercises}
-      p3={parts[2].name} e3={parts[2].exercises} />
-      <Total e1={parts[0].exercises} e2={parts[1].exercises} e3={parts[2].exercises}/>
+      p1={course.parts[0].name} e1={course.parts[0].exercises} 
+      p2={course.parts[1].name} e2={course.parts[1].exercises}
+      p3={course.parts[2].name} e3={course.parts[2].exercises} />
+      <Total e1={course.parts[0].exercises} e2={course.parts[1].exercises} e3={course.parts[2].exercises}/>
     </div>
   )
 }
